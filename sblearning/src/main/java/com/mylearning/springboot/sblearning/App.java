e4a10bb4bfd29f05cea2789ea71e920c7420e149
Hello <Hello@HELLO-PC>
2018-05-14 00:30:35 +0530

    first commit

---
+++ sblearning/src/main/java/com/mylearning/springboot/sblearning/App.java
@@ -0,0 +1,13 @@
+package com.mylearning.springboot.sblearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
